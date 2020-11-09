8ee775623cdb980014fec02d89d0403d4b22a6f8
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-11-09 13:56:02 -0600

    Create Euler 3.py

---
+++ Euler 3.py
@@ -0,0 +1,20 @@
+from math import *
+factors = []
+pFactors = []
+num = 600851475143
+div = 1
+while div < sqrt(num):
+    if (num % div == 0):
+        factors.append(div)
+    div += 1
+div = 1
+for i in factors:
+    isPrime = True
+    while div < sqrt(i):
+        if i % div == 0:
+            isPrime = False
+        div += 1
+    if(isPrime):
+        pFactors.append(i)
+        print(i)
+print(max(pFactors))
